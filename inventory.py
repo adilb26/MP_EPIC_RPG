ee6818221f6b66667d70d8839a2d7e1a87a1bd52
ADIL BELIM <adilbelim@192.168.1.9>
2025-03-18 17:20:45 +0530

    change the project format

--- inventory.py
+++
@@ -1,9 +0,0 @@
-from utils import item_emojis
-
-def view_inventory(user):
-    if not user['inventory']:
-        print("Your inventory is empty.")
-    else:
-        print("Inventory:")
-        for item, qty in user['inventory'].items():
-            print(f"{item_emojis.get(item, '')} {item}: {qty}")
--- profile.py
+++
@@ -1,9 +0,0 @@
-from utils import get_level
-
-def view_profile(user):
-    print("--------------------------------------------------")
-    print(f"👤 {user['username']}")
-    print(f"⭐ Level: {get_level(user['xp'])}")
-    print(f"⚔️ Attack: {user['attack']} | 🛡️ Defense: {user['defense']}")
-    print(f"❤️ {user['health']}/{user['total_health']} | 💰 Coins: {user['coin']} | 📈 XP: {user['xp']}")
-    print("--------------------------------------------------")
